d31d8cb3a1cecf2ca631a2dde287df2f780d650e
Sandra Grande <langeng.sandra@gmail.com>
2025-06-11 20:26:29 +0200

    fixed fun fact section

--- POR2/src/pages/Home.tsx
+++ POR2/src/pages/Home.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <p className="text-center text-xl py-3">I am currently looking for a job as a designer or a developer!</p>
       <div> 
       </div>
-        <h2 className='text-center text-3xl font-bold mt-30 mb-10'>Take a look at my recent projects!</h2>
+        <h2 className='text-center text-3xl font-bold mt-24 mb-10'>Take a look at my recent projects!</h2>
       <div className="grid md:grid-cols-3 gap-6">
         <ProjectCard
           image="/veloura.png"
@@ -37,8 +37,8 @@ export default function Home() {
         <h3 className="text-center mt-20 mb-5 font-bold text-3xl">My experience and skills</h3>
         <p className="text-center text-xl py-3 mb-5">I have studied UI-design and Frontend-development at Noroff</p>
         <p className="text-center text-xl py-3 mb-5">I have experience with HTML, CSS, Tailwind CSS, SASS, SCSS, Bootstrap, Typescript, React, Javascript, REST API, Vite, Git and much more!</p>
-        <p className="text-center text-xl py-3 mb-5">I have a passion for creating aesthetic and functional user interfaces, and I am always eager to learn new technologies and improve my skills.</p>
         <p className="text-center text-xl py-3 mb-5">I also have experience with Adobe XD, Photoshop, Illustrator, Figma, User-centered design, WCAG standards, Wireframing, Design systems and more!</p>
+        <p className="text-center text-xl py-3 mb-5">I have a passion for creating aesthetic and functional user interfaces, and I am always eager to learn new technologies and improve my skills.</p>
         <p className="text-center text-xl py-3 mb-5">I have worked on several projects, both individually and in teams, where I have applied my skills to create responsive and user-friendly web applications.</p>
       </div>
 
@@ -60,41 +60,37 @@ export default function Home() {
       </div>
 
       <div className="flex flex-col gap-10">
-        <h3 className="text-center mt-20 mb-15 font-bold text-3xl">Congrats! You made it this far. Let me tell you some fun facts about me! Yay</h3>
-        <div className="flex flex-row items-center gap-10 m-auto">
+        <h3 className="text-center mt-36 mb-12 font-bold text-3xl">Congrats! You made it this far. Let me tell you some fun facts about me! Yay</h3>
+        <div className="flex flex-col md:flex-row items-center gap-10 m-auto">
             <div className="w-80 h-60">
             <img src="/horsie.jpg" alt="Horse" className="mb-3 h-[100%] w-[100%] object-cover rounded-2xl" />
-
             </div>
-            <p>I have ridden horses since i was 7/8 years old. I have had my own for 10 years, which
+            <p className="w-80">I have ridden horses since i was 7/8 years old. I have had my own for 10 years, which
                 i have trained myself and competed nationally with! 
             </p>
         </div>
-        <div className="flex flex-row items-center gap-10 m-auto">
+        <div className="flex flex-col md:flex-row items-center gap-10 m-auto">
             <div className="w-80 h-60">
-            <img src="/zelda-painting.jpg" alt="Zelda art" className="mb-3 h-[100%] w-[100%] object-contain rounded-2xl" />
-
+            <img src="/zelda-painting.jpg" alt="Zelda art" className="mb-3 h-[100%] w-[100%] object-cover rounded-2xl" />
             </div>
-            <p>I love painting and drawing, but i dont do this too often. I usually sit 
+            <p className="w-80">I love painting and drawing, but i dont do this too often. I usually sit 
                 down and cant stop until i have finished the piece, this one took me 18h! 
             </p>
         </div>
-        <div className="flex flex-row items-center gap-10 m-auto">
+        <div className="flex flex-col md:flex-row items-center gap-10 m-auto">
             <div className="w-80 h-60">
             <img src="/bouldering.jpg" alt="Horse" className="mb-3 h-[100%] w-[100%] object-cover rounded-2xl" />
-
             </div>
-            <p>I recently started bouldering, which is a type of rock climbing.
-                I have always been afraid of heights, but this has helped me overcome that fear.
+            <p className="w-80">I recently started bouldering, which is a type of rock climbing.
+                I have always been afraid of heights, but this has helped me overcome that fear
                 (to a certain point) 
             </p>
         </div>
-        <div className="flex flex-row items-center gap-10 m-auto">
+        <div className="flex flex-col md:flex-row items-center gap-10 m-auto">
             <div className="w-80 h-60">
             <img src="/helicopter-flying.jpg" alt="Horse" className="mb-3 h-[100%] w-[100%] object-cover rounded-2xl" />
-
             </div>
-            <p>I have flown a helicopter! (with a pilot watching me of course). This was super cool!
+            <p className="w-80">I have flown a helicopter! (with a pilot watching me of course). This was super cool!
                 My dad works as a mechanic on helicopters, so i got to try it out.
             </p>
         </div>
